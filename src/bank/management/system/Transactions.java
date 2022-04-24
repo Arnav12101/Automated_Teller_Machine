ccc5392cf68edc30caa0628ad241a57708df1b7e
Arnav Parmar <63486503+Arnav12101@users.noreply.github.com>
2022-04-24 18:12:52 +0530

    Create Transactions.java

---
+++ src/bank/management/system/Transactions.java
@@ -0,0 +1,90 @@
+package bank.management.system;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Transactions extends JFrame implements ActionListener{
+    
+    JButton deposit, withdrawl, ministatement, pinchange, fastcash, balanceenquiry, exit;//MAKING ALL THE BUTTON VARIABLES AS GLOBAL
+    public Transactions()//CONSTRUCTOR
+    {
+
+        setLayout(null);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(900,900, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0,0, 900,900);
+        add(image);
+        
+        JLabel text = new JLabel("Please select your transaction");
+        text.setBounds(220,300,700,35);
+//        add(text);//THIS WILL PUT THE JLABEL ON THE FRAME BUT NOT ON THE IMAGE
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font("System", Font.BOLD, 16));
+        setUndecorated(true);
+        image.add(text);
+        
+        
+        deposit = new JButton("Deposit");
+        deposit.setBounds(170,415,150,30);
+        deposit.setBackground(Color.WHITE);
+        deposit.addActionListener(this);
+        image.add(deposit);
+        
+        withdrawl = new JButton("Cash Withdrawl");
+        withdrawl.setBounds(355, 415,150,30);
+        withdrawl.setBackground(Color.WHITE);
+        withdrawl.addActionListener(this);
+        image.add(withdrawl);
+        
+        fastcash = new JButton("Fast-Cash");
+        fastcash.setBounds(170,450,150,30);
+        fastcash.setBackground(Color.WHITE);
+        fastcash.addActionListener(this);
+        image.add(fastcash);
+        
+        ministatement = new JButton("Mini Statement");
+        ministatement.setBounds(355, 450,150,30);
+        ministatement.setBackground(Color.WHITE);
+        ministatement.addActionListener(this);
+        image.add(ministatement);
+        
+        pinchange = new JButton("Pin Change");
+        pinchange.setBounds(170,485,150,30);
+        pinchange.setBackground(Color.WHITE);
+        pinchange.addActionListener(this);
+        image.add(pinchange);
+        
+        balanceenquiry = new JButton("Balance Enquiry");
+        balanceenquiry.setBounds(355, 485,150,30);
+        balanceenquiry.setBackground(Color.WHITE);
+        balanceenquiry.addActionListener(this);
+        image.add(balanceenquiry);
+        
+        exit = new JButton("Exit");
+        exit.setBounds(355, 520,150,30);
+        exit.setBackground(Color.WHITE);
+        exit.addActionListener(this);
+        image.add(exit);
+        
+        
+        setSize(900,900);
+        setLocation(300,0);
+        setVisible(true);
+        
+    }
+    
+    public void actionPerformed(ActionEvent ae)
+    {
+        if(ae.getSource() == exit)
+        {
+            System.exit(0);
+        }
+    }
+    
+    public static void main(String args[])
+    {
+        new Transactions();
+    }
+}
