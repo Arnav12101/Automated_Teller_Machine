d5fd0fe0526cbd41c023332ad69fd13b4e9a0722
Arnav Parmar <63486503+Arnav12101@users.noreply.github.com>
2022-04-25 23:01:55 +0530

    Update SignupThree.java

--- src/bank/management/system/SignupThree.java
+++ src/bank/management/system/SignupThree.java
@@ -232,6 +232,9 @@ public class SignupThree extends JFrame implements ActionListener{
                     
                     
                     JOptionPane.showMessageDialog(null, "Card Number: " + cardnumber + "\n" + "Pin: " + pinnumber);
+                    
+                    setVisible(false);
+                    new Deposit(pinnumber).setVisible(false);
                 }
             }
             catch(Exception e)
@@ -241,7 +244,8 @@ public class SignupThree extends JFrame implements ActionListener{
         }
         else if(ae.getSource() == cancel)
         {
-            
+            setVisible(false);
+            new Login().setVisible(true);
         }
     }
     public static void main(String args[])
