931e772167332048def9214583697b9394e413c8
Arnav Parmar <63486503+Arnav12101@users.noreply.github.com>
2022-04-26 20:24:18 +0530

    Update Transactions.java

--- src/bank/management/system/Transactions.java
+++ src/bank/management/system/Transactions.java
@@ -97,6 +97,11 @@ public class Transactions extends JFrame implements ActionListener{
             setVisible(false);
             new FastCash(pinnumber).setVisible(true);
         }
+        else if(ae.getSource() == pinchange)
+        {
+            setVisible(false);
+            new PinChange(pinnumber).setVisible(true);
+        }
     }
     
     public static void main(String args[])
